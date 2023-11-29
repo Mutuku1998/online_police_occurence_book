5d0a9a3a3c91591670641b9a7571459ee108b538
mutuku1998 <martinmutuku1998@gmail.com>
2023-11-29 10:25:20 +0300

    Update ReportingForm.jsx

--- policeob/frontend/src/conponents/pages/ReportingForm.jsx
+++ policeob/frontend/src/conponents/pages/ReportingForm.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import head from "../../assets/images/s5_logo.png";
 const ReportingForm = () => {
   return (
-    <section>
+    <section className=" px-5  md:px-0 mb-10">
       <div>
-        <img src={head} alt="" className="h-20 w-full" />
+        <img src={head} alt="" className=" w-full" />
       </div>
       <div
-        className="flex text-center justify-between flex-col mt-10 shadow-2xl w-md h-full bg-green-200  
+        className=" w-full md:flex text-center justify-between flex-col mt-10 shadow-2xl w-md h-full bg-green-200  
                     rounded-lg "
       >
         <div className=" flex items-center flex-col ">
@@ -19,7 +19,7 @@ const ReportingForm = () => {
           </p>
         </div>
         <form className="w-full mt-5">
-          <div className="flex justify-between">
+          <div className="flex-col-2 md:flex justify-between">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -29,16 +29,12 @@ const ReportingForm = () => {
               </label>
               <input
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                
                 type="text"
                 required
               />
             </div>
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-               
-              >
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Middle Name
               </label>
               <input
@@ -78,7 +74,7 @@ const ReportingForm = () => {
             </div>
           </div>
 
-          <div className="flex justify-between">
+          <div className=" flex-col-2 md:flex justify-between">
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Contact
@@ -94,27 +90,21 @@ const ReportingForm = () => {
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 for="grid-city"
               >
-               Town
+                Town
               </label>
               <input
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        
                 type="text"
                 required
               />
             </div>
             <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              
-              >
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Region
               </label>
               <div className="relative">
-                <select
-                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-             
-                >
+                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                  <option>Select Region</option>
                   <option>Nairobi</option>
                   <option>Central</option>
                   <option>Coast</option>
@@ -124,23 +114,157 @@ const ReportingForm = () => {
                   <option>Nyanza</option>
                   <optin>Rift Valley</optin>
                 </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"></div>
+            
               </div>
             </div>
             <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-             
-              >
-              Postal address
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Postal address
               </label>
               <input
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                
                 type="text"
-                
               />
             </div>
+          </div>
+          <div className="flex-col-2 md:flex justify-between mt-3">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Report Date
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                type="date"
+                required
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Report Time
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                type="time"
+                required
+              />
+            </div>
+            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Crime Category
+              </label>
+              <div className="relative">
+                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                  <option>select category</option>
+                  <option>Theft</option>
+                  <option>Corruption</option>
+                  <option>Murder</option>
+                  <option>Drug abuse</option>
+                  <option>Human trafficking</option>
+                  <option>Child abuse</option>
+                  <option>Gender Violence</option>
+                  <optin>Terrorisms</optin>
+                  <option>Others</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"></div>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Damages
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                required
+              />
+            </div>
+          </div>
+          <div className="flex-col-2 md:flex justify-between mt-3">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Date of Incident
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                type="date"
+                required
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Time of Incident
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                type="time"
+                required
+              />
+            </div>
+
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Location (provide specific details)
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                required
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Witness
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                required
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Upload Evidence
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                type="file"
+                required
+              />
+            </div>
+          </div>
+          <div className=" flex-col-2 md:flex justify-between mt-3">
+<div className=" md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                for="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+              Description
+              </label>
+              <textarea
+                id="description"
+                rows="7"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write the description here..."
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="  w-xl md:w-full mt-8">
+            <button type="submit" className="bg-blue-600 text-white rounded  h-16 w-1/3 text-center text-[18px]">
+                Submit
+            </button>
           </div>
         </form>
       </div>
